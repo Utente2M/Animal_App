619e057bb0948424d7459996d7b2057413fdad20
M Murdaca <m.murdaca1@studenti.uniba.it>
2022-12-01 19:46:22 +0100

    Creazione di sistema di auteticazione email e password

--- app/src/main/java/it/uniba/dib/sms222315/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222315/MainActivity.java
@@ -2,7 +2,9 @@ package it.uniba.dib.sms222315;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lunchLogin(View view) {
+        Intent login_prova = new Intent(this, EmailPasswordActivity.class);
+        startActivity(login_prova);
     }
 }
