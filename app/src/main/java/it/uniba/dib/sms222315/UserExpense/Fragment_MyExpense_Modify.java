9b6c2ccff12cfe09ad70fb56dd623ae8e30dfcf5
M Murdaca <m.murdaca1@studenti.uniba.it>
2022-12-17 15:29:53 +0100

    punto di ripristino

--- app/src/main/java/it/uniba/dib/sms222315/UserExpense/Fragment_MyExpense_Modify.java
+++ app/src/main/java/it/uniba/dib/sms222315/UserExpense/Fragment_MyExpense_Modify.java
@@ -8,60 +8,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import it.uniba.dib.sms222315.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_MyExpense_Modify#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_MyExpense_Modify extends Fragment {
 
+    //INIZIALIZZIAMO I CONTROLLI PER AGGIUNGERE SPESE
+    Spinner SpinCategory; //spin_Category_MyExpense
+    String sendModCategory;
+    EditText ET_modValue; //ET_decimal_MyExpense
+    EditText ET_modDescr; //ET_descr_MyExpense
+    Button BT_modExp; //BT_create_MyExpense
 
     private static final String TAG = "TAG_Frag_MyExpense_MODIFY";
     MyExpense receivedExpense;
 
 
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Fragment_MyExpense_Modify() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_MyExpense_Modify.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_MyExpense_Modify newInstance(String param1, String param2) {
-        Fragment_MyExpense_Modify fragment = new Fragment_MyExpense_Modify();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
         //LOAD DATA FROM FRAG MY EXPENSE HOME
         Bundle bundle = this.getArguments();
         if (bundle != null) {
@@ -78,8 +57,19 @@ public class Fragment_MyExpense_Modify extends Fragment {
         Log.d(TAG , "onCreateView ");
         View my_view = inflater.inflate(R.layout.fragment__my_expense__modify , container , false);
 
-
+        //da eliminare a fine scrittura
         Log.d(TAG, "receivedExpense : " + receivedExpense.getPrv_Category_MyExpense());
+
+        BT_modExp = my_view.findViewById(R.id.BT_MOD_MyExpense);
+        BT_modExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "button conferm modify");
+            }
+        });
+
+
+
 
         return my_view;
     }
