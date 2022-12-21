710a68fea4b30c8b8cb993fcc59a1fc30994076f
M Murdaca <m.murdaca1@studenti.uniba.it>
2022-12-21 18:34:25 +0100

    prova massi no pull

--- app/src/main/java/it/uniba/dib/sms222315/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222315/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,12 +19,21 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import it.uniba.dib.sms222315.Autentication.Fragment_Login;
 import it.uniba.dib.sms222315.Autentication.LoginOrRegisterActivity;
 import it.uniba.dib.sms222315.TestListView.Activity_MemberList;
+import it.uniba.dib.sms222315.UserExpense.Fragment_MyExpense_Home;
+import it.uniba.dib.sms222315.UserPets.Fragment_MyPets_Home;
 import it.uniba.dib.sms222315.UserProfile.ProfileUserActivity;
 import it.uniba.dib.sms222315.oldFile.User_information_Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Fragment my_fragment;
+    FragmentManager my_frag_manager;
+    FragmentTransaction my_frag_trans;
+    FragmentManager my_frag_manager_menu;
+    FragmentTransaction my_frag_trans_menu;
 
     private static final String TAG = "TAG_MainActivity";
     // [START declare_auth]
@@ -35,19 +47,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        addFragment();
+        addFragmentHome();
+
         //toolbar set as app bar
     //   Toolbar myToolbar = findViewById(R.id.toolbar);
       //  setSupportActionBar(myToolbar);
-        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
-        findViewById(R.id.ImageMenu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                drawerLayout.openDrawer(GravityCompat.START);
-
-            }
-        });
 
 
         // Initialize Firebase Auth
@@ -58,6 +65,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     }//END OnCreate
+
+    private void addFragmentHome() {
+
+        Fragment_MyPets_Home my_fragment = new Fragment_MyPets_Home();
+
+
+        //my_fragment.setMy_callbackFragment(this);
+
+        my_frag_manager = getSupportFragmentManager();
+        my_frag_trans = my_frag_manager.beginTransaction();
+
+        my_frag_trans.add(R.id.FramContHome , my_fragment);
+        my_frag_trans.commit();
+    }
+
+    private void addFragment() {
+        MenuBar_FullUser my_fragment_menu = new MenuBar_FullUser();
+        my_frag_manager_menu = getSupportFragmentManager();
+        my_frag_trans_menu = my_frag_manager_menu.beginTransaction();
+        my_frag_trans_menu.add(R.id.FragContMenuBar , my_fragment_menu);
+        my_frag_trans_menu.commit();
+    }
 
     @Override
     protected void onStart() {
