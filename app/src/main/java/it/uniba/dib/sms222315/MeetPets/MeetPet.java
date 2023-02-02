7eeaf8f2586a8a58d9a722468ce739c56dca49a5
M Murdaca <m.murdaca1@studenti.uniba.it>
2023-02-02 21:39:17 +0100

    punto ripristino massi ok qrcode

---
+++ app/src/main/java/it/uniba/dib/sms222315/MeetPets/MeetPet.java
@@ -0,0 +1,16 @@
+package it.uniba.dib.sms222315.MeetPets;
+
+public class MeetPet {
+    private String prv_uidPet;
+
+    public MeetPet(String uidPet) {
+        prv_uidPet =uidPet;
+    }
+    public MeetPet(){
+
+    }
+
+    public String getPrv_uidPet() {
+        return prv_uidPet;
+    }
+}
